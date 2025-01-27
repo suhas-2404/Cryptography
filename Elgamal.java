b20fe00535c7386f6b8a6867e4c0dbe2a1e07c89
unknown <suhasrajeshmysore@gmail.com>
2025-01-27 19:47:36 +0530

    updated Elgamal

--- Elgamal.java
+++ Elgamal.java
@@ -1,11 +1,9 @@
 import java.math.BigInteger;
-import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Elgamal {
-    private static final SecureRandom random = new SecureRandom();
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
